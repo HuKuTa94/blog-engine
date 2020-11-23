73ba222f352ec9a966833c0a61782fb7da03fc0a
Nikita Koshelev <niki-koshel@yandex.ru>
2020-11-23 18:56:53 +0500

    added global settings DB migrations

---
+++ src/main/resources/db/migration/U1.1__Add_data_in_table_global_settings.sql
@@ -0,0 +1,3 @@
+DELETE FROM `blogengine`.`global_settings` WHERE code = 'MULTIUSER_MODE';
+DELETE FROM `blogengine`.`global_settings` WHERE code = 'POST_PREMODERATION';
+DELETE FROM `blogengine`.`global_settings` WHERE code = 'STATISTICS_IS_PUBLIC';
